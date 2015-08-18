d573eb268cd6d4fca1526fe570a897e0f2c5f386
egordina <jgarm@yandex.ru>
2015-08-18 17:19:02 +0300

    login page and js server dummies

---
+++ assets/js/server.js
@@ -0,0 +1,23 @@
+/**
+ * Created with JetBrains PhpStorm.
+ * User: EGordina
+ * Date: 18.08.15
+ * Time: 14:55
+ * To change this template use File | Settings | File Templates.
+ */
+/*
+ *
+ */
+function login(){
+}
+
+function logout(){
+}
+
+function getLists(){
+}
+
+function getListItems(listId){
+}
+
+
